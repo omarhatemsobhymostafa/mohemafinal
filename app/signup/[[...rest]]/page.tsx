f43c01aa0ce548a1b1a8da2a6f822693d465400e
omarhatemsobhymostafa <omar.hatem.okasha@gmail.com>
2026-09-26 19:20:45 +0300

    changing in some text in signup page 2

--- app/signup/[[...rest]]/page.tsx
+++ app/signup/[[...rest]]/page.tsx
@@ -4,8 +4,11 @@ export default function SignUpPage() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-50">
       <SignUp
+        routing="path"
+        path="/signup"
         signInUrl="/signin"
         forceRedirectUrl="/"
+        signInForceRedirectUrl="/"
         appearance={{
           elements: {
             rootBox: "w-full max-w-md",
